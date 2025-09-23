906f96937091b4065d1978703ee8bdb56c8badab
mikael <mikael.st15@protonmail>
2025-09-23 18:30:59 -0300

    refator: changing migrations sequence / updating README

--- README.md
+++ README.md
@@ -11,6 +11,12 @@ Esse repositório contém código da API para a aplicação Tasker, sistema de a
 ```bash
 # Database Configuration
 DB_URL = "postgresql://USUARIO:SENHA@HOST:PORTA/NOME_DO_BANCO?schema=public"
+DB_USER = {seu_usuario}
+DB_PASS = {sua_senha}
+DB_NAME = {nome_do_banco}
+DB_HOST = {host/localhost}
+DB_PORT = {porta}
+DB_DIALECT = postgres
 #API Keys
 API_PORT = {porta}
 SECRET = "sua_key"
---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config(); // carrega o .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || 'postgres', // ou mysql, mariadb, sqlite
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+};
