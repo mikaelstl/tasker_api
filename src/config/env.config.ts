f10b633f0b23721325f2ed07adfe83d0cf9962e9
mikael <mikael.st15@protonmail>
2025-09-23 17:52:47 -0300

    refactor: adjusting README with project infos

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,9 +5,5 @@ dotenv.config();
 const DB_URL = process.env.DB_URL as string;
 const API_PORT = process.env.API_PORT || 0 as number;
 const SECRET = process.env.SECRET as string;
-const STATE = process.env.STATE as string;
 
-// console.log('uri: ',    DB_URL);
-// console.log('secret: ', SECRET);
-
-export { DB_URL, API_PORT, SECRET, STATE };
+export { DB_URL, API_PORT, SECRET };
