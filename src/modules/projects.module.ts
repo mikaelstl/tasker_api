5082d64a6ce6742e4e41b4c0937357899842d143
mikael <mikael.st15@protonmail>
2025-11-08 10:29:01 -0300

    feat: adding Events and Comments controllers to ProjectModule

--- src/modules/projects.module.ts
+++ src/modules/projects.module.ts
@@ -4,6 +4,7 @@ import { EventsRepository } from "@repositories/events.repository";
 import { ProjectMemberRepository } from "@repositories/member.repository";
 import { TasksRepository } from "@repositories/tasks.repository";
 import { ProjectService } from "@services/project.service";
+import { CommentsController } from "src/controller/comment.controller";
 import { EventsController } from "src/controller/events.controller";
 import { ProjectController } from "src/controller/project.controller";
 import { TasksController } from "src/controller/task.controller";
@@ -13,7 +14,8 @@ import { ProjectRepository } from "src/repositories/projects.repository";
   controllers: [
     ProjectController,
     TasksController,
-    EventsController
+    EventsController,
+    CommentsController
   ],
   providers: [
     ProjectRepository,
