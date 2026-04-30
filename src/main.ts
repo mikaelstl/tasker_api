a67728a89bc379b3e65e6847c6545e73e97c3fe2
mikael <mikael.st15@protonmail.com>
2026-04-30 11:54:05 -0300

    refactor: changing API_PORT env variable to PORT / removing cors setup

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { API_PORT } from './config/env.config';
 import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './utils/filters/exception.filter';
-import { ResponseInterceptor } from './utils/filters/response.filter';
-import { ValidationExceptionFilter } from '@filters/validation.filter';
+import { HttpExceptionFilter } from './common/filters/exception.filter';
+import { ResponseInterceptor } from './common/filters/response.filter';
+import { ValidationExceptionFilter } from 'src/common/filters/validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  })
-
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
